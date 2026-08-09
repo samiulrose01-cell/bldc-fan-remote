e759c20a91f4a5572878f121c0cd21a49f7b07e2
samiulrose01-cell <samiul.rose01@gmail.com>
2026-08-09 11:48:32 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="fan-remote-v1";self.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
