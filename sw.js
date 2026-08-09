5993df4722fcd6e103edaf66e4e67ca685754942
samiulrose01-cell <samiul.rose01@gmail.com>
2026-08-09 22:22:42 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
-const C="fan-remote-v1";self.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
+const CACHE="fan-remote-v2";
+const CORE=["./","./index.html","./manifest.json","./sw.js","./icon-512.png"];
+
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET") return;
+  const url=new URL(event.request.url);
+  if(url.origin===location.origin){
+    event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(r=>{
+      const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r;
+    }).catch(()=>caches.match("./index.html"))));
+  }
+});
