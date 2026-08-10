ef9228ebdb55af9798fcfa2470dcfd3af94d27a4
samiulrose01-cell <samiul.rose01@gmail.com>
2026-08-10 07:53:08 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const C="fan-remote-v1";self.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
+const C="fan-remote-v1";const HOME_IP = "192.168.10.141";
+
+function openHomeRemote(){
+  window.location.href = "http://" + HOME_IP + "/";
+}sself.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
